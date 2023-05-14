95b1be37597f5ac44bad227cdf3c4e0b6564519b
Matteo-Tech <matteo.guglilemi.ma.gu@gmail.com>
2023-05-14 18:15:58 +0200

    🔧 add: custom indent and tab obtions for python

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,28 +1,28 @@
 local status_ok, which_key = pcall(require, "which-key")
 if not status_ok then
-    return
+  return
 end
 
 -- Advanced pyright configuration
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright", "jsonls" })
 
 local pyright_opts = {
-    single_file_support = true,
-    settings = {
-        pyright = {
-            disableLanguageServices = false,
-            disableOrganizeImports = false,
-        },
-        python = {
-            analysis = {
-                autoImportCompletions = true,
-                autoSearchPaths = true,
-                diagnosticMode = "workspace", -- openFilesOnly, workspace
-                typeCheckingMode = "basic", -- off, basic, strict
-                useLibraryCodeForTypes = true,
-            },
-        },
+  single_file_support = true,
+  settings = {
+    pyright = {
+      disableLanguageServices = false,
+      disableOrganizeImports = false,
     },
+    python = {
+      analysis = {
+        autoImportCompletions = true,
+        autoSearchPaths = true,
+        diagnosticMode = "workspace", -- openFilesOnly, workspace
+        typeCheckingMode = "basic", -- off, basic, strict
+        useLibraryCodeForTypes = true,
+      },
+    },
+  },
 }
 
 require("lvim.lsp.manager").setup("pyright", pyright_opts)
@@ -31,7 +31,7 @@ require("lvim.lsp.manager").setup("pyright", pyright_opts)
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup { { name = "black" }, filetypes = { "python" }, extra_args = { "--fast", "--line-length=80" } }
 formatters.setup {
-    { name = "autoflake", filetypes = { "python" }, extra_args = { "--in-place", "--remove-all-unused-imports" } },
+  { name = "autoflake", filetypes = { "python" }, extra_args = { "--in-place", "--remove-all-unused-imports" } },
 }
 formatters.setup { { name = "reorder_python_imports", filetypes = { "python" } } }
 
@@ -40,42 +40,52 @@ local linters = require "lvim.lsp.null-ls.linters"
 linters.setup { { command = "pydocstyle", filetypes = { "python" } } }
 
 local opts = {
-    mode = "n", -- NORMAL mode
-    prefix = "<leader>",
-    buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-    silent = true, -- use `silent` when creating keymaps
-    noremap = true, -- use `noremap` when creating keymaps
-    nowait = true, -- use `nowait` when creating keymaps
+  mode = "n", -- NORMAL mode
+  prefix = "<leader>",
+  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+  silent = true, -- use `silent` when creating keymaps
+  noremap = true, -- use `noremap` when creating keymaps
+  nowait = true, -- use `nowait` when creating keymaps
 }
 
 -- setup debug adapter
 lvim.builtin.dap.active = true
 local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
 pcall(function()
-    require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
+  require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
 end)
 
 -- setup testing
 require("neotest").setup {
-    adapters = {
-        require "neotest-python" {
-            -- Extra arguments for nvim-dap configuration
-            -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
-            dap = {
-                justMyCode = false,
-                console = "integratedTerminal",
-            },
-            args = { "--log-level", "DEBUG", "--quiet" },
-            runner = "pytest",
-        },
+  adapters = {
+    require "neotest-python" {
+      -- Extra arguments for nvim-dap configuration
+      -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
+      dap = {
+        justMyCode = false,
+        console = "integratedTerminal",
+      },
+      args = { "--log-level", "DEBUG", "--quiet" },
+      runner = "pytest",
     },
+  },
 }
 
 local mappings = {
-    C = {
-        name = "Python",
-        c = { "<cmd>lua require('swenv.api').pick_venv()<cr>", "Choose Env" },
-    },
+  C = {
+    name = "Python",
+    c = { "<cmd>lua require('swenv.api').pick_venv()<cr>", "Choose Env" },
+  },
 }
 
 which_key.register(mappings, opts)
+
+--------- EDITOR SETTINGS ---------
+local py_opts = {
+  shiftwidth = 4, -- the number of spaces inserted for each indentation
+  tabstop = 4, -- insert 2 spaces for a tab
+}
+
+for k, v in pairs(py_opts) do
+  vim.opt[k] = v
+end
