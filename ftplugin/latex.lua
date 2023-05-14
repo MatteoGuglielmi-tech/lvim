f8225a3f6c23f5c3e7b5dced1b29d65944edf83c
Matteo-Tech <matteo.guglilemi.ma.gu@gmail.com>
2023-05-14 18:14:21 +0200

    🔧 add: new code actions for latex

--- ftplugin/latex.lua
+++ ftplugin/latex.lua
@@ -1,45 +1,48 @@
 local texlab_opts = {
-    cmd = { "texlab" },
-    settings = {
-        texlab = {
-            build = {
-                args = {
-                    "-pdf",
-                    "-interaction=nonstopmode",
-                    "-synctex=1",
-                    "%f",
-                },
-                executable = "latexmk",
-                onSave = true,
-            },
-            chktex = {
-                onEdit = true,
-                onOpenAndSave = false,
-            },
-            forwardSearch = {
-                executable = "zathura",
-                args = { "--synctex-forward", "%1:1:%f", "%p" },
-            },
-            lint = {
-                onChange = true,
-            },
-            latexFormatter = "latexindent",
-            latexindent = {
-                modifyLineBreaks = true,
-            },
+  cmd = { "texlab" },
+  settings = {
+    texlab = {
+      build = {
+        args = {
+          "-pdf",
+          "-interaction=nonstopmode",
+          "-synctex=1",
+          "%f",
         },
+        executable = "latexmk",
+        onSave = true,
+      },
+      chktex = {
+        onEdit = true,
+        onOpenAndSave = false,
+      },
+      forwardSearch = {
+        executable = "zathura",
+        args = { "--synctex-forward", "%1:1:%f", "%p" },
+      },
+      lint = {
+        onChange = true,
+      },
+      latexFormatter = "latexindent",
+      latexindent = {
+        modifyLineBreaks = true,
+      },
     },
+  },
 
-    filetypes = { "tex", "bib", "plaintex" },
+  filetypes = { "tex", "bib", "plaintex" },
 }
 
-local marksmen_opts = {
-    cmd = { "marksmen" },
-    filetypes = { "tex", "bib", "plaintex" },
+local marksman_opts = {
+  cmd = { "marksman" },
+  filetypes = { "tex", "bib", "plaintex" },
 }
 
 require("lvim.lsp.manager").setup("textlab", texlab_opts)
-require("lvim.lsp.manager").setup("marksmen", marksmen_opts)
+require("lvim.lsp.manager").setup("marksman", marksman_opts)
 
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup { { command = "cspell", filetypes = { "markdown", "tex" } } }
+
+local code_actions = require "lvim.lsp.null-ls.code_actions"
+code_actions.setup { { command = "cspell", args = { "%file" }, filetypes = { "markdown", "tex" } } }
