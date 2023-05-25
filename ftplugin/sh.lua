c4c0984cf6388ae6a94b57cfc36d2d0eec21c148
Matteo-tech <matteo.guglielmi.ma.gu@gmail.com>
2023-05-25 18:35:35 +0200

    fix(bash): address faulty linters and formatters
    - Move Lsp configuration to ftplugin sh.lua file
    - Push zsh extension to filetypes
    - Fix linters and formatters. It seems like ftplugin/sh.lua was colliding with config
      in lua/user/languages/sh.lua file
    - Fix file name bash.lua -> sh.lua

---
+++ ftplugin/sh.lua
@@ -0,0 +1,41 @@
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+vim.opt.cmdheight = 1
+
+vim.filetype.add {
+  extension = {
+    zsh = "zsh",
+  },
+}
+
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "bashls" })
+
+local lsp_manager = require "lvim.lsp.manager"
+lsp_manager.setup("bashls", {
+  filetypes = { "sh", "zsh" },
+  on_init = require("lvim.lsp").common_on_init,
+  capabilities = require("lvim.lsp").common_capabilities(),
+})
+
+lsp_manager.setup("lua_ls", {
+  filetypes = { "lua" },
+  on_init = require("lvim.lsp").common_on_init,
+  capabilities = require("lvim.lsp").common_capabilities(),
+})
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup { { command = "shfmt", filetypes = { "sh", "zsh" } } }
+
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  { command = "shellcheck", filetypes = { "sh", "zsh" } },
+}
+
+local code_actions = require "lvim.lsp.null-ls.code-actions"
+code_actions.setup {
+  {
+    command = "shellcheck",
+    filetypes = { "sh", "zsh" },
+    argss = { "--format", "json1", "--source-path=$DIRNAME", "--external-sources", "-" },
+  },
+}
