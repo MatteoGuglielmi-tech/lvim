992be1b15d0659fbbf2ccbcb0fddad36942758d1
Matteo-tech <matteo.guglielmi.ma.gu@gmail.com>
2023-05-25 18:29:43 +0200

    refactor(formatters): move formatters to ftplugin
    All formatters to corresponding file in ftplugin folder. I do like more
    when servers loads when necessary for current buffers.

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -2,13 +2,6 @@ vim.diagnostic.config { virtual_text = false }
 
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "jdtls" })
 
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  { command = "stylua", filetypes = { "lua" } },
-  { command = "shfmt", filetypes = { "sh", "zsh" } },
-  { command = "prettier", filetypes = { "css" } },
-}
-
 lvim.format_on_save = true
 -- lvim.lsp.on_attach_callback = function(client, bufnr)
 -- end
